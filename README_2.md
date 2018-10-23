b009a8d26b46f14ce5f74b4942f2ce052e957dec
Wane Smith <wanesmith7@gmail.com>
2018-10-23 00:51:46 -0400

    file changes

--- README_2.md
+++ README_2.md
@@ -4,7 +4,7 @@
  
  The goal was trying out some new ways of using the conditional statements without using the same examples that can be found in almost all the beginner tutorials. In the beginning, the examples would confuse me because I didn't understand how they would connect together.
  
-  Understanding and experimenting with what I've learned shows me the output of the progress from all the time and hours spent into learning the fundamentals.
+  Understanding and experimenting with what I've learned shows me the output of the progress from all the time and hours spent into learning the fundamentals. Still a long way to go.
   
    Creating a simple background helps me to stay creative for front end purposes. My preference is CSS, HTML, and Javascript from scratch, because of learning.
 
--- build/javascript/jscript.js
+++ build/javascript/jscript.js
@@ -16,14 +16,14 @@ Simple conditional check: For example, if you change any text or the name in the
 The example below checks out if you enter the correct answer. Else, you get the alert of the next condition. This is used to check two conditions. */
 
 
-//  let name = prompt('what is the programming language for web development?');
+ let name = prompt('what is the programming language for web development?');
 
-//  if(name === 'javascript'){
-//     // alert('Correct answer is Javascript');
-//     alert(`Correct answer is ${name}`);
-//  }else{
-//     alert(`Incorrect answer try again`);
-// }
+ if(name === 'javascript'){
+    // alert('Correct answer is Javascript');
+    alert(`Correct answer is ${name}`);
+ }else{
+    alert(`Incorrect answer try again`);
+}
 
 /*Example: 3 
 The example below just combines some examples of different code that I've learned, on my adventure. The goal is creating some basic beginner friendly examples, that displays how they can be used together. */
@@ -33,10 +33,11 @@ The example below just combines some examples of different code that I've learne
 // let color1 = 'green';
 // let color2 = 'sienna';
 
+
+
 // let websiteBackground = document.querySelector('body');
 
 // if(color === 'green'){
-    // alert('Correct answer is Javascript');
 
 //    alert(`You prefer ${color1} for the background`);
 //     websiteBackground.style.backgroundColor='green';
@@ -49,46 +50,67 @@ The example below just combines some examples of different code that I've learne
 // }else{
 //     alert('Sorry :-(  The value does not match the available options. :-)');
 
-    // websiteBackground.style.backgroundColor='darkgrey';
+//     websiteBackground.style.backgroundColor='darkgrey';
 // }
 
 // setTimeout (()=> 
 // alert('This is Javascript!....{;-)'), 1500);
 
 /*Example:4 */
-let color = prompt('What color do you prefer for the background color? black,blue,grey,purple,red?');
+// let color = prompt('What color do you prefer for the background color? black,blue,grey,purple,red?');
 
-let color1 = 'red';
-let color2 = 'blue';
-let color3 = 'purple';
-let color4 = 'grey';
-let color5 = 'black;'
+// let color1 = 'red';
+// let color2 = 'blue';
+// let color3 = 'purple';
+// let color4 = 'grey';
+// let color5 = 'black;'
 
-let websiteBackground = document.querySelector('body');
+// let websiteBackground = document.querySelector('body');
 
-if(color==='red'){
-    // alert('Correct answer is Javascript');
-    alert(`You prefer ${color1} for the background`);
-    websiteBackground.style.backgroundColor='red';
- }else if(color === 'blue'){
-    alert(`You prefer ${color2} for the background`);
-    websiteBackground.style.backgroundColor='blue';
-}else if(color === 'purple'){
-    alert(`You prefer ${color3} for the background`);
-    websiteBackground.style.backgroundColor='purple';
-}
-else if(color === 'grey'){
-    alert(`You prefer ${color4} for the background`);
-    websiteBackground.style.backgroundColor='grey';
-}
-else if(color === 'black'){
-    alert(`You prefer ${color5} for the background`);
-    websiteBackground.style.backgroundColor='black';
-}else{
-    alert('Sorry :-(  The value does not match the available options. :-)');
-    websiteBackground.style.backgroundColor='orangered';
-}
+// if(color==='red'){
+//   
+//     alert(`You prefer ${color1} for the background`);
+//     websiteBackground.style.backgroundColor='red';
+//  }else if(color === 'blue'){
+//     alert(`You prefer ${color2} for the background`);
+//     websiteBackground.style.backgroundColor='blue';
+// }else if(color === 'purple'){
+//     alert(`You prefer ${color3} for the background`);
+//     websiteBackground.style.backgroundColor='purple';
+// }
+// else if(color === 'grey'){
+//     alert(`You prefer ${color4} for the background`);
+//     websiteBackground.style.backgroundColor='grey';
+// }
+// else if(color === 'black'){
+//     alert(`You prefer ${color5} for the background`);
+//     websiteBackground.style.backgroundColor='black';
+// }else{
+//     alert('Sorry :-(  The value does not match the available options. :-)');
+//     websiteBackground.style.backgroundColor='orangered';
+// }
 
-setTimeout (()=> 
-alert('This is Javascript...{;-)'), 1500);
+// setTimeout (()=> 
+// alert('This is Javascript...{;-)'), 1500);
+
+/*Example: 5
+ */
+//  let age = prompt('How old are you?');
+
+//  if(age<=19){
+//     alert('You are a teenager that likes software engineering.');
+// }else if(age<=25){
+//     alert('You are young and like software engineering.');
+    
+// }else if(age<=30){
+//     alert('You are a young adult that likes software engineering.');
+   
+// }else if(age<=50){
+//     alert('You are a mature adult that likes software engineering.');
+   
+// }else if (age<=60){
+//     alert('You are an experienced adult that likes software engineering.');
+// }else{
+//     alert('You are fully developed and love software engineering.');
+// }
 
